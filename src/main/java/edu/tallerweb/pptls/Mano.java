9bd27441f68ab576856ac75d78c1d19f26432f2c
pablohaylan <pablo8809@hotmail.com>
2013-10-02 19:19:22 -0300

    Implementacion de codigo

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -4,14 +4,14 @@ package edu.tallerweb.pptls;
  * Representa una de las Manos involucradas en el juego
  */
 public class Mano {
-
+private Forma miForma;
 	/**
 	 * Toda Mano debe crearse con una forma dada, que será
 	 * la que determine su condición en el juego.
 	 * @param forma, la Forma que adopta la Mano.
 	 */
 	public Mano(final Forma forma) {
-		throw new RuntimeException("No implementado aún");
+		this.miForma = forma;
 	}
 
 	/**
@@ -21,7 +21,53 @@ public class Mano {
 	 * @return un Resultado, de acuerdo al estado del juego.
 	 */
 	public Resultado jugarCon(final Mano otra) {
-		throw new RuntimeException("No implementado aún");
+		Resultado resultadoPartida=Resultado.PIERDE;
+		
+		if(this.miForma==otra.miForma){
+			resultadoPartida = Resultado.EMPATA;
+		}
+		
+		
+		
+		switch (this.miForma) 
+        { 
+            case PIEDRA: if (otra.miForma == Forma.TIJERA || otra.miForma == Forma.LAGARTO ) {
+            	
+            	resultadoPartida = Resultado.GANA;
+            	
+            }
+                break; 
+
+            case SPOCK: if (otra.miForma == Forma.PIEDRA || otra.miForma == Forma.TIJERA){
+            	
+            	resultadoPartida = Resultado.GANA;            	
+            } 
+
+                break; 
+
+            case PAPEL: if (otra.miForma == Forma.SPOCK || otra.miForma == Forma.PIEDRA){
+            	
+            	resultadoPartida = Resultado.GANA;            	
+            } 
+
+                break;               
+
+            case LAGARTO: if (otra.miForma == Forma.PAPEL || otra.miForma == Forma.SPOCK){
+            	
+            	resultadoPartida = Resultado.GANA;            	
+            } 
+
+                break; 
+                
+            case TIJERA: if (otra.miForma == Forma.LAGARTO || otra.miForma == Forma.PAPEL){
+            	
+            	resultadoPartida = Resultado.GANA;            	
+            } 
+
+                break;     
+        } 	
+		
+		return  resultadoPartida;
 	}
 
 }
